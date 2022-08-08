9b0b5d8af2504d67da2edf4a301db2465a33e454
Ardian Pradipta <rdnpradipta@gmail.com>
2022-08-08 21:15:18 +0700

    bump version

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'flag-icon-css/css/flag-icon.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'flag-icons/css/flag-icons.min.css'
 import '@vueform/toggle/themes/default.css'
-import "./assets/css/custom.css";
+import "./assets/css/custom.css"
 import i18n from './i18n'
 
 createApp(App).use(i18n).use(i18n).mount('#app')
